b3c4c6c852e1eb020aff29c66a1585105c03eeba
Kuba Podkomórka <jpodkomorka19@gmail.com>
2023-11-24 03:53:36 +0100

    Added types to the particle fields in __init__ method

--- particle.py
+++ particle.py
@@ -8,9 +8,9 @@ from sys import float_info
 
 class Particle:
     def __init__(self, position: Vector2, velocity: Vector2):
-        self.position = position
-        self.velocity = velocity
-        self.personal_best = Vector2(0, float_info.max)
+        self.position: Vector2 = position
+        self.velocity: Vector2 = velocity
+        self.personal_best: Vector2 = Vector2(0, float_info.max)
 
     def __str__(self) -> str:
         return f'Particle position: {self.position}, velocity: {self.velocity}'
