6e29eba4391f0136840212ab77c9da161e66e784
Kuba Podkomórka <jpodkomorka19@gmail.com>
2023-11-24 03:52:34 +0100

    Overriden default operators to work on vectors as needed. Updated the tests

--- tests/test_vector2.py
+++ tests/test_vector2.py
@@ -4,10 +4,38 @@ from vector2 import random_vector_in_range, Vector2
 
 class TestVector2(TestCase):
     def setUp(self):
-        pass
+        self.vector1 = Vector2(-1.0, -1.0)
+        self.vector2 = Vector2(1.0, 1.0)
+
+    def test_init(self):
+        self.assertEqual(self.vector1.x, -1.0)
+        self.assertEqual(self.vector1.y, -1.0)
+        self.assertEqual(self.vector2.x, 1.0)
+        self.assertEqual(self.vector2.y, 1.0)
+
+    def test_add(self):
+        result = self.vector1 + self.vector2
+        self.assertEqual(result.x, 0.0)
+        self.assertEqual(result.y, 0.0)
+
+    def test_sub(self):
+        result = self.vector1 - self.vector2
+        self.assertEqual(result.x, -2.0)
+        self.assertEqual(result.y, -2.0)
+
+    def test_mul(self):
+        result = self.vector1 * 10
+        self.assertEqual(result.x, -10.0)
+        self.assertEqual(result.y, -10.0)
 
     def test_random_vector_in_range(self):
-        random_vector = random_vector_in_range(4, 5)
+        result = random_vector_in_range(4, 5)
+        self.assertTrue(4 <= result.x <= 5)
+        self.assertTrue(4 <= result.y <= 5)
+
+    def test_random_vector_in_range_raises_exception(self):
+        with self.assertRaises(Exception):
+            random_vector_in_range(100, 1)
 
 
 if __name__ == '__main__':
--- vector2.py
+++ vector2.py
@@ -7,8 +7,17 @@ import random
 
 class Vector2:
     def __init__(self, x: float, y: float):
-        self.x = x
-        self.y = y
+        self.x: float = x
+        self.y: float = y
+
+    def __add__(self, other):
+        return Vector2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector2(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return Vector2(self.x * other, self.y * other)
 
     def __str__(self) -> str:
         return f'Vector2({self.x, self.y})'
